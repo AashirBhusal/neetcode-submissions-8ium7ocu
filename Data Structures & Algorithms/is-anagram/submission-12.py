0a808eb2d4d663c26f4fc8dfaeb43aa387c31fd6
Aashir bhusal <86728775+AashirBhusal@users.noreply.github.com>
2026-09-18 18:42:48 +1000

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
+
+        for i in s:
+            if i not in s_dict:
+                s_dict[i] = 1
+            else:
+                s_dict[i] += 1
+
+
+        for i in t:
+            if i not in t_dict:
+                t_dict[i] = 1
+            else:
+                t_dict[i] += 1
+
+        if t_dict == s_dict:
+            return True
+        else:
+            return False
